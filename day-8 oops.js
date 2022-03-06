f1969695c8dfb6a997dbb3f969b5556cf1110fc0
sabarinathanN <98639811+sabarinathanN@users.noreply.github.com>
2022-03-06 19:03:42 +0530

    Add files via upload

---
+++ day-8 oops.js
@@ -0,0 +1,12 @@
+//Write a “person” class to hold all the details.
+
+class person {
+    constructor(name,uniqueid,college,department){
+      this.name=name;
+      this.uniqueid=uniqueid;
+      this.college=college;
+      this.department=department;
+    }
+  }
+    var s1=new person("sabari","1234","MKCE","EEE");
+  console.log(s1);
---
+++ day8 uber.js
@@ -0,0 +1,14 @@
+//calculate uber price
+
+class person {
+    constructor(name,uberid,kmnoted,perkmcharge,totalcost){
+      this.name=name;
+      this.uberid=uberid;
+      this.kmnoted=kmnoted;
+      this.perkmcharge=perkmcharge;
+     
+    }
+  }
+    var s1=new person("sabari","1234",20*13.50,13.50);
+  
+  console.log(s1);
